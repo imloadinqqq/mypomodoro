556c71222e301f21944dec0f852f531ceeb15b64
Alex Diaz <imloadinq7579@gmail.com>
2025-07-03 17:40:08 -0500

    close db connection on exit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from pathlib import Path
 from PyQt6.QtWidgets import QApplication
 from ui.main_window import MainWindow
+from db import conn
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -9,3 +10,5 @@ if __name__ == "__main__":
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
+
+    conn.close()
